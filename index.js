e7e4f10e930a17f5fde3b0a56a122d825309ac79
dante <maxgamerdu@gmail.com>
2021-11-12 13:07:58 +0300

    added a few variables as well as set up a main function

---
+++ index.js
@@ -0,0 +1,11 @@
+function output(){
+    var day=document.getElementById("day").nodeValue;
+    var dd=parseInt(day);
+  var month=document.getElementById("month").nodeValue;
+  var mm=parseInt(month);
+  var year=document.getElementById("year").nodeValue;
+  var yy=parseInt(year);
+  var cc=(yy-1)/100+1;
+  var result=parseInt(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+  var boy=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  var girl=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
